2885f65990eda8f1e38049ccc4a18bf6f531851c
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-05-13 17:58:16 +0200

    add strawberry fields

--- StrawberryFields.java
+++ StrawberryFields.java
@@ -5,7 +5,13 @@ public class StrawberryFields {
     //Al final está la letra completa
     
     public static void main(String[] args) {
-
+    for (int i = 1; i <= 7; i++){
+        if (i % 2 !=0){
+            playChorus();
+        } else {
+            playVersus(i);
+        }
+    }
     }
 
     /**
@@ -16,7 +22,30 @@ public class StrawberryFields {
      * Inside the function:
      * 1. print versus according the position number
      */
-    
+
+    public static void playVersus(int number){
+        switch (number){
+            case 2:
+                System.out.println("Living is easy with eyes closed\n" +
+                        "Misunderstanding all you see\n" +
+                        "It's getting hard to be someone, but it all works out\n" +
+                        "It doesn't matter much to me\n");
+                break;
+            case 4:
+                System.out.println("No one I think is in my tree\n" +
+                        "I mean, it must be high or low\n" +
+                        "That is, you can't, you know, tune in, but it's alright\n" +
+                        "That is, I think it's not too bad\n");
+                break;
+            case 6:
+                System.out.println("Always, no sometimes, think it's me\n" +
+                        "But you know, I know when it's a dream\n" +
+                        "I think I know, I mean a yes\n" +
+                        "But it's all wrong\n" +
+                        "That is, I think I disagree\n");
+                break;
+        }
+    }
 
     /**
      * Function name: playChorus
@@ -24,10 +53,15 @@ public class StrawberryFields {
      * Inside the function:
      * 1. print chorus 
      */
-    
 
+    public static void playChorus(){
+        System.out.println("Let me take you down\n" +
+                "'Cause I'm going to strawberry fields\n" +
+                "Nothing is real\n" +
+                "And nothing to get hung about\n" +
+                "Strawberry fields forever\n");
+    }
 }
-
 
 /* 
 Strawberry Fields Forever
